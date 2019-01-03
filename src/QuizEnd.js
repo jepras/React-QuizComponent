4d0a71018e4f61f720faf7ff7c6a3264497f2976
jepras <jeprasher@gmail.com>
2019-01-03 10:26:26 +0100

    fix

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 
-export default class QuizEnd extends Component {
+class QuizEnd extends Component {
   render() {
     return (
       <div>
         <p>Thanks for playing!</p>
-        <a href="#">Reset Quiz</a>
+        <a href="">Reset Quiz</a>
       </div>
     );
   }
 }
+
+export default QuizEnd;
